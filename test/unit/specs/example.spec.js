a0f10a879ca203cab04f92bf9c14027e3c968de6
tianzhi <1095098525@qq.com>
2020-01-07 10:09:12 +0800

    test

--- test/unit/specs/example.spec.js
+++ test/unit/specs/example.spec.js
@@ -2,14 +2,14 @@
  * @Author: tianzhi
  * @Date: 2020-01-06 18:30:56
  * @LastEditors  : tianzhi
- * @LastEditTime : 2020-01-06 23:29:08
+ * @LastEditTime : 2020-01-07 10:08:36
  */
 
 /**
  *  在package.json文件中配置   husky 可以在提交的时候自动执行单元测试
  */
 function add(a, b) {
-    return a+b
+    return a+b+1
 }
 
 describe('测试例子', () => {
